145fb4e35bf1d68f0a219c7edb10728a2c16741e
Erik Ribeiro <erik@ribeiro.com>
2024-09-15 00:16:51 +0100

    fix vela atrasada di

--- testes_iniciais/pybot.py
+++ testes_iniciais/pybot.py
@@ -63,13 +63,19 @@ class ADXIndicator(bt.Indicator):
         self.plus_di = bt.indicators.PlusDI(self.data, period=adxLength)
         self.minus_di = bt.indicators.MinusDI(self.data, period=adxLength)
 
+    def next(self):
+        # Usar os valores "atrasados" no cálculo para evitar atraso de 1 vela
+        self.lines.adx[0] = self.adx[-1]
+        self.lines.plus_di[0] = self.plus_di[-1]
+        self.lines.minus_di[0] = self.minus_di[-1]
+
 # Configurando a estratégia do Backtrader apenas para calcular o ADX e os DI's
 class ADXStrategy(bt.Strategy):
     def __init__(self):
         self.adx_indicator = ADXIndicator(self.data)
 
     def next(self):
-        # Aqui você pode acessar os valores de ADX, DI+ e DI- e armazená-los para uso posterior
+        # Acessar os valores ajustados de ADX, DI+ e DI-
         self.adx_value = self.adx_indicator.adx[0]
         self.plus_di_value = self.adx_indicator.plus_di[0]
         self.minus_di_value = self.adx_indicator.minus_di[0]
