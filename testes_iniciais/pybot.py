add74e694e33e83f9ac1de3dbc4d47df5870cd66
bcastrodias <b.castro.doute@gmail.com>
2024-09-15 16:34:14 -0300

    desbug

--- testes_iniciais/pybot.py
+++ testes_iniciais/pybot.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # Remove or comment out this section if you have your own data.
 
 
-df = pd.read_csv('testes_iniciais/BYBIT_BTCUSDT.P_1h.csv')
+df = pd.read_csv('testes_iniciais/data.csv')
 df['time'] = pd.to_datetime(df['time'])
 df = df.set_index('time')
 
