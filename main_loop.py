1c80d371b50039b65744ad7471babe0b2d20afff
Erik Ribeiro <erik@ribeiro.com>
2024-09-16 16:56:27 +0100

    add: start_lateral

--- main_loop.py
+++ main_loop.py
@@ -10,14 +10,14 @@ if os.path.isfile(trade_history_file):
     df_trade_history = pd.read_csv(trade_history_file)
     
     # Verificar se a coluna 'type' já existe
-    if 'type' not in df_trade_history.columns:
+    if 'entry_lateral' not in df_trade_history.columns:
         # Adicionar a nova coluna 'type' com valores em branco
-        df_trade_history['type'] = ''
+        df_trade_history['entry_lateral'] = ''
         
         # Salvar o CSV com a nova coluna
         df_trade_history.to_csv(trade_history_file, index=False)
-        print("Coluna 'type' adicionada com sucesso.")
+        print("Coluna 'entry_lateral' adicionada com sucesso.")
     else:
-        print("A coluna 'type' já existe no arquivo.")
+        print("A coluna 'entry_lateral' já existe no arquivo.")
 else:
     print(f"O arquivo {trade_history_file} não foi encontrado.")
