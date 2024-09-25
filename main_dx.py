3d7c8875f3fffc7364c1331aaf0ba62a8264124b
Erik Ribeiro <erik@ribeiro.com>
2024-09-25 17:23:58 +0100

    cleaning github

--- main_dx.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('BYBIT_BTCUSDT.P.csv')
-df.to_csv('/app/data/BYBIT_BTCUSDT.P.csv', index=False)
--- src/indicators/volume.py
+++
@@ -1,4 +0,0 @@
-def is_above_average(data, period=14):
-    if data['volume'].iloc[-2] > data['volume'].rolling(period).mean().iloc[-2]:
-        return True
-    return False
