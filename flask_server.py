98171deba27caa1c34827589eeb3cd4b2d2e5b1a
Erik Ribeiro <erik@ribeiro.com>
2024-09-27 16:50:30 +0100

    add: API Bruno

--- flask_server.py
+++ flask_server.py
@@ -23,7 +23,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv('SECRET_KEY', '1221')  # Padrão para '1221' se não estiver definido
 
 
-accounts_order = ['FERNANDO', 'PABLO', 'HAMUCHY', 'ZE', 'NATAN', 'ERIK']
+accounts_order = ['FERNANDO', 'PABLO', 'HAMUCHY', 'ZE', 'NATAN', 'BRUNO', 'ERIK']
 
 # Dicionário para armazenar as sessões da API e dados da conta
 api_sessions = {}
